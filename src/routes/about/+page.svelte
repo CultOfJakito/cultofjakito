4b48c6eaa798884755182c6f319a22386b85b8b8
Yuvraj Verma <vermayuvraj2010@gmail.com>
2023-04-01 23:57:38 +0530

    here you go waffle

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,24 +3,129 @@
 	<meta name="description" content="About this app" />
 </svelte:head>
 
-<div class="text-column">
-	<h1>About this app</h1>
+<script lang="ts">
 
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
+	
+	 
+</script>
 
-	<pre>npm create svelte@latest</pre>
+<div class="w-full mt-20">
+	<section class="w-full">
+		<h1 class="m-auto text-center text-3xl">Meet The Team</h1>
 
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
+		<div class="flex w-4/5 justify-center m-auto flex-col">
+			
+			<div class="flex justify-evenly w-full mt-12">
+				
+				<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+					<div class="flex flex-col items-center pb-10">
+						<img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="" alt="ZedDev"/>
+						<h5 class="mb-1 text-xl font-medium text-gray-900 ">ZedDev</h5>
+						<span class="text-sm text-gray-500 ">Developer</span>
+						<p class="text-sm text-gray-500">Currently a college student and active developer on the team</p>
+					</div>
+				</div>
 
-	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
-	</p>
+				<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+					<div class="flex flex-col items-center pb-10">
+						<img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="" alt="ZedDev"/>
+						<h5 class="mb-1 text-xl font-medium text-gray-900 ">ZedDev</h5>
+						<span class="text-sm text-gray-500 ">Developer</span>
+						<p class="text-sm text-gray-500">Currently a college student and active developer on the team</p>
+					</div>
+				</div>
+
+				<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+					<div class="flex flex-col items-center pb-10">
+						<img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="" alt="ZedDev"/>
+						<h5 class="mb-1 text-xl font-medium text-gray-900 ">ZedDev</h5>
+						<span class="text-sm text-gray-500 ">Developer</span>
+						<p class="text-sm text-gray-500">Currently a college student and active developer on the team</p>
+					</div>
+				</div>
+			</div>
+			
+			<div class="flex justify-evenly w-full mt-12">
+				
+				<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+					<div class="flex flex-col items-center pb-10">
+						<img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="" alt="ZedDev"/>
+						<h5 class="mb-1 text-xl font-medium text-gray-900 ">ZedDev</h5>
+						<span class="text-sm text-gray-500 ">Developer</span>
+						<p class="text-sm text-gray-500">Currently a college student and active developer on the team</p>
+					</div>
+				</div>
+
+				<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+					<div class="flex flex-col items-center pb-10">
+						<img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="" alt="ZedDev"/>
+						<h5 class="mb-1 text-xl font-medium text-gray-900 ">ZedDev</h5>
+						<span class="text-sm text-gray-500 ">Developer</span>
+						<p class="text-sm text-gray-500">Currently a college student and active developer on the team</p>
+					</div>
+				</div>
+
+				<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+					<div class="flex flex-col items-center pb-10">
+						<img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="" alt="ZedDev"/>
+						<h5 class="mb-1 text-xl font-medium text-gray-900 ">ZedDev</h5>
+						<span class="text-sm text-gray-500 ">Developer</span>
+						<p class="text-sm text-gray-500">Currently a college student and active developer on the team</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="flex justify-evenly w-full mt-12">
+				
+				<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+					<div class="flex flex-col items-center pb-10">
+						<img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="" alt="ZedDev"/>
+						<h5 class="mb-1 text-xl font-medium text-gray-900 ">ZedDev</h5>
+						<span class="text-sm text-gray-500 ">Developer</span>
+						<p class="text-sm text-gray-500">Currently a college student and active developer on the team</p>
+					</div>
+				</div>
+
+				<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+					<div class="flex flex-col items-center pb-10">
+						<img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="" alt="ZedDev"/>
+						<h5 class="mb-1 text-xl font-medium text-gray-900 ">ZedDev</h5>
+						<span class="text-sm text-gray-500 ">Developer</span>
+						<p class="text-sm text-gray-500">Currently a college student and active developer on the team</p>
+					</div>
+				</div>
+
+				<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+					<div class="flex flex-col items-center pb-10">
+						<img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="" alt="ZedDev"/>
+						<h5 class="mb-1 text-xl font-medium text-gray-900 ">ZedDev</h5>
+						<span class="text-sm text-gray-500 ">Developer</span>
+						<p class="text-sm text-gray-500">Currently a college student and active developer on the team</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="flex justify-evenly w-full mt-12">
+				
+				<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+					<div class="flex flex-col items-center pb-10">
+						<img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="" alt="Bonnie"/>
+						<h5 class="mb-1 text-xl font-medium text-gray-900 ">ZedDev</h5>
+						<span class="text-sm text-gray-500 ">Developer</span>
+						<p class="text-sm text-gray-500">Currently a college student and active developer on the team</p>
+					</div>
+				</div>
+
+
+			</div>
+
+		</div>
+	</section>
+
+
 </div>
+
+<style lang="postcss">
+
+
+</style>
--- src/routes/components/Header.svelte
+++ src/routes/components/Header.svelte
@@ -1,17 +1,30 @@
 <script>
 	import { page } from '$app/stores';
+
+	import github from '$lib/images/github-mark-white.svg'
+	import youtube from '$lib/images/youtube.svg'
 </script>
 
-<header class="w-full">
-	<nav class="w-full flex flex-row align-middle items-center justify-center">
-		<ul>
+<header class="w-full pt-3">
+	<navbar class="w-full flex flex-row items-center justify-evenly ">
+		<ul class="w-full flex flex-row items-center justify-evenly">
 			<li class="inline" aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
 			<li class="inline" aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
 			</li>
+			<li class="inline" aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+				<a href="/about">Projects</a>
+			</li>
+			<li class="inline" aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+				<a href="/about">Events</a>
+			</li>
 		</ul>
-	</nav>
+		<ul class="w-1/6 flex flex-row items-center justify-start">
+			<li class="inline mr-4"><a href="https://github.com/CultOfJakito"><img class="h-8 w-8" src={github} alt=""></a></li>
+			<li class="inline"><a href="https://www.youtube.com/channel/UCU531rHxXbIcDG4u_K_QbEw"><img class="h-min w-8" src={youtube} alt=""></a></li>
+		</ul>
+	</navbar>
 </header>
 
